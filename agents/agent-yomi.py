23178ed74b382ec5005f03e8e491aa355e15cc72
Sherif Eid <sherif.eid@gmail.com>
2023-03-30 17:48:43 -0700

    use only influxdb2

--- agents/agent-yomi.py
+++ agents/agent-yomi.py
@@ -5,12 +5,10 @@ import time
 from sensirion_i2c_driver import I2cConnection, LinuxI2cTransceiver
 from sensirion_i2c_sen5x import Sen5xI2cDevice
 import signal
-from influxdb import InfluxDBClient as InfluxDBClient_v1
 import logging
 import logging.handlers
 import coloredlogs
 import json
-
 import os
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -22,13 +20,6 @@ print(requests.certs.where())
 token = os.getenv("INFLUX_TOKEN")
 org = "home"
 bucket = "yomi"
-
-USER = 'admin'
-PASSWORD = 'XXXX'
-DBNAME = 'yomi'
-HOST = 'poolpi'
-PORT = 8086
-POLLTIME = 3
 
 # check if first argument exists, will be the config file
 log_filename = 'mylog.log'
@@ -76,70 +67,61 @@ with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
             logging.debug("Waiting for new data...")
             while device.read_data_ready() is False:
                 time.sleep(0.1)
-
+            
+            store = False
             # Read measured values -> clears the "data ready" flag
             values = device.read_measured_values()
 
             current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-            point = {
-                "measurement": 'YomiSEN55',
-                "time": current_time,
-                "fields": {}
-            }
-
-            point2 = Point("YomiSEN55")
-            point2.time(datetime.utcnow(), WritePrecision.NS)
+            point = Point("YomiSEN55")
+            point.time(datetime.utcnow(), WritePrecision.NS)
 
             if (values.mass_concentration_1p0.available):
                 if (values.mass_concentration_1p0.physical != 0.0):
-                    point['fields']['mc1p0'] = values.mass_concentration_1p0.physical
-                    point2.field("mc1p0", values.mass_concentration_1p0.physical)
+                    point.field("mc1p0", values.mass_concentration_1p0.physical)
+                    store = True
             if (values.mass_concentration_1p0.available):
                 if (values.mass_concentration_2p5.physical != 0.0):
-                    point['fields']['mc2p5'] = values.mass_concentration_2p5.physical
-                    point2.field('mc2p5', values.mass_concentration_2p5.physical)
+                    point.field('mc2p5', values.mass_concentration_2p5.physical)
+                    store = True
             if (values.mass_concentration_1p0.available):
                 if (values.mass_concentration_4p0.physical != 0.0):
-                    point['fields']['mc4p0'] = values.mass_concentration_4p0.physical
-                    point2.field('mc4p0', values.mass_concentration_4p0.physical)
+                    point.field('mc4p0', values.mass_concentration_4p0.physical)
+                    store = True
             if (values.mass_concentration_1p0.available):
                 if (values.mass_concentration_10p0.physical != 0.0):
-                    point['fields']['mc10p0'] = values.mass_concentration_10p0.physical
-                    point2.field('mc10p0', values.mass_concentration_10p0.physical)
+                    point.field('mc10p0', values.mass_concentration_10p0.physical)
+                    store = True
 
             if (values.ambient_humidity.available):
-                point['fields']['rh'] = values.ambient_humidity.percent_rh
-                point2.field('rh', values.ambient_humidity.percent_rh)
+                point.field('rh', values.ambient_humidity.percent_rh)
+                store = True
 
             if (values.ambient_temperature.available):
-                point['fields']['temp_c'] = values.ambient_temperature.degrees_celsius
-                point['fields']['temp_f'] = values.ambient_temperature.degrees_fahrenheit
-                point2.field('temp_c', values.ambient_temperature.degrees_celsius)
-                point2.field('temp_f', values.ambient_temperature.degrees_fahrenheit)
+                point.field('temp_c', values.ambient_temperature.degrees_celsius)
+                point.field('temp_f', values.ambient_temperature.degrees_fahrenheit)
+                store = True
 
             if (values.voc_index.available):
-                point['fields']['voc_index'] = values.voc_index.scaled
-                point2.field('voc_index', values.voc_index.scaled)
+                point.field('voc_index', values.voc_index.scaled)
+                store = True
 
             if (values.nox_index.available):
-                point['fields']['nox_index'] = values.nox_index.scaled
-                point2.field('nox_index', values.nox_index.scaled)
+                point.field('nox_index', values.nox_index.scaled)
+                store = True
 
             logging.debug('values: \n{}'.format(values))
 
-            if bool(point['fields']):
-                logging.info('influxdb point: \n{}'.format(json.dumps(point, indent=4)))
-                client = InfluxDBClient_v1(HOST, PORT, USER, PASSWORD, DBNAME)
-                client.switch_database(DBNAME)
-                client.write_points([point])
-                with InfluxDBClient(url="https://influx.elnamla.com:8086", token=token, org=org) as client2:
-                    write_api = client2.write_api(write_options=SYNCHRONOUS)
-                    write_api.write(bucket, org, point2)
-                client2.close()
+            if (store):
+                logging.info('push to influxDB2')
+                with InfluxDBClient(url="https://influx.elnamla.com:8086", token=token, org=org) as client:
+                    write_api = client.write_api(write_options=SYNCHRONOUS)
+                    write_api.write(bucket, org, point)
+                client.close()
 
             # Read device status
             status = device.read_device_status()
-            logging.info("Device Status: {}\n".format(status))
+            logging.info("Device Status: {}".format(status))
 
     except KeyboardInterrupt:
         logging.info("Received keyboard interrupt. Exiting...")
